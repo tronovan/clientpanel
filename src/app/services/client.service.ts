8f7b7355143732a470ebe8a830cb9ed32b0c2af3
Donovan Venuto <tronovan@gmail.com>
2018-06-16 10:54:33 -0500

    More imports.

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ClientService {
