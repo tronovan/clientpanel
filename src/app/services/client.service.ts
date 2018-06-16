001e55c6144479fd59cbc3d8f5494ee22a1e7cf9
Donovan Venuto <tronovan@gmail.com>
2018-06-16 11:50:02 -0500

    Corrected capitalization error.

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
-import { Client } from '../Models/Client';
+import { Client } from '../models/Client';
 
 @Injectable()
 export class ClientService {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA_T9VTRgkHuH-jUjAuF7xxEBi3zIOC9VQ",
+    authDomain: "clientpanelprod-1fea8.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-1fea8.firebaseio.com",
+    projectId: "clientpanelprod-1fea8",
+    storageBucket: "clientpanelprod-1fea8.appspot.com",
+    messagingSenderId: "265566601727"
+  }
 };
