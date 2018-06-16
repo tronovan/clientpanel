c10b202ab743bb6e58a2121c1da91f436c49d907
Donovan Venuto <tronovan@gmail.com>
2018-06-16 11:05:01 -0500

    Built getClients method.

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -9,6 +9,20 @@ export class ClientService {
   clientDoc: AngularFirestoreDocument<Client>;
   clients: Observable<Client[]>;
   client: Observable<Client>;
-  constructor() { }
+  constructor(private afs: AngularFirestore) { 
+    this.clientsCollection = this.afs.collection('clients', ref => ref.orderBy('lastName', 'asc'));
+  }
+  getClients(): Observable<Client[]>{
+    //Get clients with the id.
+    this.clients = this.clientsCollection.snapshotChanges()
+    .map(changes => {
+      return changes.map(action => {
+        const data = action.payload.doc.data() as Client;
+        data.id = action.payload.doc.id;
+        return data;
+      });
+    });
+    return this.clients;
+  }
 
 }
