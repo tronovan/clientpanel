77047e6cb5256a68db4d614a7925572be84b5e18
Donovan Venuto <tronovan@gmail.com>
2018-06-16 10:29:17 -0500

    Set up routes to the different components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AddClientComponent } from './components/add-client/add-client.component';
 import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { ClientDetailsComponent} from './components/client-details/client-details.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '',  component: DashboardComponent },
+  { path: 'login',  component: LoginComponent },
+  { path: 'register',  component: RegisterComponent },
+  { path: 'client/add',  component: AddClientComponent },
+  { path: 'client/edit/:id',  component: EditClientComponent },
+  { path: 'client/:id',  component: ClientDetailsComponent },
+  { path: 'settings',  component: SettingsComponent },
+  { path: '**',  component: NotFoundComponent }
+];
 
 @NgModule({
+  exports: [RouterModule],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ],
   declarations: []
 })
