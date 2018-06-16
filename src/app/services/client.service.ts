4572509ee91883027513959d8532dc0faba8815d
Donovan Venuto <tronovan@gmail.com>
2018-06-16 10:58:02 -0500

    Added class members to the client service.

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { Client } from '../Models/Client';
 
 @Injectable()
 export class ClientService {
-
+  clientsCollection: AngularFirestoreCollection<Client>;
+  clientDoc: AngularFirestoreDocument<Client>;
+  clients: Observable<Client[]>;
+  client: Observable<Client>;
   constructor() { }
 
 }
